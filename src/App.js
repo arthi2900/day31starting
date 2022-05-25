be435497343198875fe5b7117e41e705480f2b86
arthi2900 <aaarthi2927@gmail.com>
2022-05-25 08:36:10 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Link, Switch, Route} from "react-router-dom";
+import { Intro } from './Intro';
+import { Login } from './Login';
+import { useState} from 'react';
+import { Data } from './Data';
+import { Viewstudent } from './Viewstudent';
+import { Editstudent } from './Editstudent';
+import { NewFunction } from './NewFunction';
+const studentdetails=[{
+  sname:"arthi",
+  degree:"BCA",
+  year:"1",
+  section:"A",
+  batchyear:"2020-2023"
+},{
+  sname:"anandhi",
+  degree:"BCA",
+  year:"1",
+  section:"B",
+  batchyear:"2020-2023"
+},
+{
+  sname:"vanitha",
+  degree:"BCA",
+  year:"2",
+  section:"A",
+  batchyear:"2021-2024"
+},
+{
+  sname:"vani",
+  degree:"BCA",
+  year:"2",
+  section:"A",
+  batchyear:"2021-2024"
+},
+{
+  sname:"renu",
+  degree:"BCA",
+  year:"3",
+  section:"A",
+  batchyear:"2020-2023"
+},
+{
+  sname:"iniya",
+  degree:"BCA",
+  year:"3",
+  section:"B",
+  batchyear:"2020-2023"
+},
+{
+  sname:"Deva",
+  degree:"BCA",
+  year:"2",
+  section:"A",
+  batchyear:"2020-2023"
+}];
 function App() {
+  const [ studentlist,setstudentlist]= useState(studentdetails);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <Intro/>
+            <div>
+      <Link to="/Home">Home  </Link>
+         <Link to="/Student">Student  </Link>
+      <Link to="/NewFunction">Add Student  </Link>
+          <Link to="/Login">Login  </Link>
+      <Switch>
+        <Route path="/Home"></Route>
+        <Route path="/Teacher"></Route>
+                <Route path="/Student/edit/:id"><Editstudent studentlist={studentlist} setstudentlist={setstudentlist}/></Route>
+        <Route path="/Student/:id"><Viewstudent studentlist={studentlist}/></Route>
+             <Route path="/Student"><Data studentlist={studentlist} setstudentlist={setstudentlist}/></Route>
+                 <Route path="/NewFunction"><NewFunction studentlist={studentlist} setstudentlist={setstudentlist} /></Route>
+        
+        <Route path="/Login"><Login/></Route>
+      </Switch>
     </div>
-  );
-}
-
-export default App;
+           </div>
+  );}
+ export default App;
